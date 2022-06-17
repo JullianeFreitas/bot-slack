c0d7a5a5bad59e19972d70cc6efe7a32e8be6e98
Julliane Freitas <jullianefreitas@gmail.com>
2022-06-17 02:41:40 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const SlackBot = require('slackbots');
+const dotenv = require('dotenv')
+const fs = require('fs')
+
+dotenv.config()
+
+const bot = new SlackBot({
+  token: `${process.env.BOT_TOKEN}`,
+  name: 'bot-slack'
+})
+
+bot.on('start', async () => {
+
+  const config = {
+    username: 'bot-name'
+  }
+  bot.postMessageToChannel(
+    `${process.env.SLACK_CHANNEL}`,
+    `Teste`,
+    config
+  );
+
+  setInterval((function () {
+    return process.exit(0);
+  }), 120000)
+  
+})
+
+process.on('exit', function (code) {
+  return console.log(`${new Date()} [INFO] About to exit with code ${code}`);
+});
+
+
+
